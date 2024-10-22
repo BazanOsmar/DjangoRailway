00599c446e23ce1544b8fbd55345a8f94b9c7827
Osmar <obazanalarcon@gmail.com>
2024-10-21 22:46:27 -0400

    Prueba 3: le cambie el nombre de la carpeta imagenes

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -82,10 +82,7 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL_POSTGREST'))
 }
 
 
