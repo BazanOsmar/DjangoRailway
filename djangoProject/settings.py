96de5c49b0a0cc46fbd0dc7f107181edc379278c
Osmar <obazanalarcon@gmail.com>
2024-10-21 22:43:23 -0400

    Prueba 2: le puse el database de defecto

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -82,7 +82,10 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL_POSTGREST'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
